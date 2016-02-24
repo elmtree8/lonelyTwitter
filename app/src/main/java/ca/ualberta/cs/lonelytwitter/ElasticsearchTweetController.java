f727c1edc1efc720fff39d1ff6d3d7a11c508348
elmtree8 <elmtree8@gmail.com>
2016-02-23 19:15:14 -0700

    lab 7 exercise

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -9,9 +9,12 @@ import com.searchly.jestdroid.JestDroidClient;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 
 /**
  * Created by esports on 2/17/16.
@@ -21,7 +24,33 @@ public class ElasticsearchTweetController {
 
     //TODO: A function that gets tweets
     public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<Tweet>> {
+        @Override
+        protected ArrayList<Tweet> doInBackground(String... search_strings) {
+            verifyClient();
 
+            // Start our initial array list (empty)
+            ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+            // NOTE: I'm making a huge assumption here, that only the first search term will be used.
+            Search search = new Search.Builder(search_strings[0])
+                .addIndex("testing").addType("tweet").build();
+            try {
+                SearchResult execute = client.execute(search);
+                if (execute.isSucceeded()) {
+                    // Return our list of tweets
+                    List<NormalTweet> returned_tweets = execute.getSourceAsObjectList(NormalTweet.class);
+                    tweets.add(returned_tweets);
+                }
+                else {
+                    // TODO: Add an error message, because this was puzzling.
+                    // TODO: Right here it will trigger if the insert fails
+                    Log.i("TODO", "We actually failed here, adding a tweet");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
     //public static ArrayList<Tweet> getTweets() {
     //    verifyClient();
@@ -54,6 +83,33 @@ public class ElasticsearchTweetController {
             }
             return null;
         }
+    }
+
+    // https://github.com/searchbox-io/Jest/blob/master/jest/README.md 2/23/16
+    protected static List stringSearch( String query) {
+        verifyClient();
+        Search search = new Search.Builder(query)
+                // multiple index or types can be added.
+                .addIndex("twitter")
+                .addIndex("tweet")
+                .build();
+
+        try {
+            SearchResult execute = client.execute(search);
+            if (execute.isSucceeded()) {
+                // Return our list of tweets
+                List<NormalTweet> returned_search = execute.getSourceAsObjectList(NormalTweet.class);
+                return returned_search;
+            }
+            else {
+                // TODO: Add an error message, because this was puzzling.
+                // TODO: Right here it will trigger if the insert fails
+                Log.i("TODO", "We actually failed here, adding a tweet");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void verifyClient() {
